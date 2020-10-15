25e258c6a18a4346b0b7d2a95d04e4abe23094dd
sti <sti@trusted.ru>
2020-10-15 20:30:48 +0300

    added typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import {
+    Text, View
+} from 'react-native';
+
+const App = () => {
+    return (
+        <View style={{ width: "100%", height: "100%", justifyContent: "center", alignItems: "center" }}>
+            <Text>Hello world!</Text>
+        </View>
+    );
+};
+
+export default App;
