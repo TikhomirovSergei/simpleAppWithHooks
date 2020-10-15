9c31d147249ff6cab2b769c275a4fe5a8a2b3d1b
sti <sti@trusted.ru>
2020-10-15 22:44:48 +0300

    added eslint-plugin-react-hooks and testing useState hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
         "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
         "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        "plugin:react-hooks/recommended",
     ],
     rules: {
         "@typescript-eslint/explicit-module-boundary-types": "off",
@@ -28,5 +29,12 @@ module.exports = {
         "@typescript-eslint/interface-name-prefix": "off",
         "@typescript-eslint/no-use-before-define": "off",
         "react/prop-types": "off",
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": [
+            "warn",
+            {
+                additionalHooks: "(useMyCustomHook|useMyOtherCustomHook)",
+            },
+        ],
     },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import * as React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button, Text, View } from "react-native";
 
 const App = () => {
+    const [count, incCount] = useState(0);
+
+    const updateCounter = () => {
+        incCount(count + 1);
+    };
+
     return (
         <View style={{ width: "100%", height: "100%", justifyContent: "center", alignItems: "center" }}>
             <Text>Hello world!</Text>
+            <Text>{count}</Text>
+            <Button title={"update"} onPress={updateCounter} />
         </View>
     );
 };
