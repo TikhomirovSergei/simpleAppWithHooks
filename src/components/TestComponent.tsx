b0350acbb6e9b42c062b04618716fa19ed8949c6
sti <sti@trusted.ru>
2020-10-17 22:56:48 +0300

    added weather api

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
 import { Button, Text, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
+import { connect } from "react-redux";
 
-import { changeCounter } from "../redux/actions/test";
+import { changeCounter, getWeather } from "../redux/actions/test";
+
+const mapDispatchToProps = (dispatch: Function) => {
+    return {
+        getWeather,
+    };
+};
 
 const TestComponent = () => {
     const counter = useSelector((state: any) => state.test.counter);
@@ -17,6 +24,10 @@ const TestComponent = () => {
         dispatch(changeCounter());
     };
 
+    const getWeatherButton = () => {
+        dispatch(getWeather());
+    };
+
     return (
         <View style={{ width: "100%", height: "100%", justifyContent: "center", alignItems: "center" }}>
             <Text>Hello world!</Text>
@@ -24,8 +35,9 @@ const TestComponent = () => {
             <Button title={"update local counter"} onPress={updateCounter} />
             <Text>{counter}</Text>
             <Button title={"update redux counter"} onPress={updatePropsCounter} />
+            <Button title={"get weather"} onPress={getWeatherButton} />
         </View>
     );
 };
 
-export default TestComponent;
+export default connect(null, mapDispatchToProps)(TestComponent);
--- src/redux/actions/test.tsx
+++ src/redux/actions/test.tsx
@@ -1,3 +1,27 @@
+import { openweathermapApi } from "../../assets/apiKey";
+import { IWeatherInfoModel } from "../../interfaces/weatherInfoModel";
+
 export const changeCounter = () => ({
     type: "CHANGE_COUNTER",
 });
+
+export function getWeather() {
+    return function action(dispatch: Function) {
+        getWeatherAPI("466806")
+            .then((data: IWeatherInfoModel) => {
+                console.log(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+}
+
+const getWeatherAPI = (cityID: string): Promise<IWeatherInfoModel> => {
+    return new Promise((resolve, reject) => {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?id=${cityID}&lang=ru&appid=${openweathermapApi}`)
+            .then((resp) => resp.json())
+            .then((data) => resolve(data))
+            .catch((err) => reject(err));
+    });
+};
